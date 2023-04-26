4bd2151afc21c3c12fa01d36de78521179336d42
ponkkis <53916568+ponkkis@users.noreply.github.com>
2023-04-26 11:22:21 +0300

    Tiedoston luku ja arvattavan sanan asetus.

--- hirsipuu.py
+++ hirsipuu.py
@@ -1,6 +1,27 @@
+import random
+
 class Hirsipuu:
     def __init__(self) -> None:
-        pass
+        self.arvattava_sana = ""
+        self.arvatut_kirjaimet = []
 
-    def __str__(self) -> str:
-        return "Hello"
+        self.alusta_peli()
+
+    def alusta_peli(self):
+        """ Lukee sanat tiedostosta listaan ja tallentaa satunnaisen sanan listasta arvattava_sana muuttujaan """
+        sanat = []
+        with open("kaikkisanat.txt", "r", encoding="utf-8") as tiedosto:
+            for rivi in tiedosto:
+                sanat.append(rivi.strip())
+        
+        self.arvattava_sana = random.choice(sanat)
+
+    def __str__(self):
+        return f"{self.arvattava_sana}"
+
+# debuggausta varten
+"""
+if True:
+    hp = Hirsipuu()
+    print(hp)
+"""
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from hirsipuu import Hirsipuu
 
 if __name__ == "__main__":
-    hp = Hirsipuu()
-    print(hp)
+    pass
