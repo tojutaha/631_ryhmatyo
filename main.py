9619e62dfd78416dc186e37380f1e370388d3d01
MrRodent <finaali@gmail.com>
2023-04-27 14:23:45 +0300

    Lisätty näytön tyhjennysmetodi sekä ohjemetodia paranneltu

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from hirsipuu import Hirsipuu
+from hirsipuucopy import Hirsipuu # muista copy
+import os
+import platform
 
 class HirsipuuSovellus:
     def __init__(self) -> None:
         self.puu = Hirsipuu()
+        self.__voittosana = ""
     
+    def tyhjenna_naytto(self):
+        """ Tyhjentää konsolin järjestelmästä riippumatta """
+        os_nimi = platform.system()
+        if os_nimi == "Windows":
+            os.system("cls")
+        else:
+            os.system("clear")
+
     def tulosta_sana(self, sana: str):
-        """ Tulostaa san"""
+        """ Tulostaa arvatut kirjaimet sekä tyhjän paikan arvaamattomille """
         for kirjain in sana:
             if kirjain in self.puu.oikein_arvatut_kirjaimet:
                 print(kirjain, " ", end="")
             else:
                 print("_ ", end="")
 
+    # TODO: tämä toimimaan
     def tulosta_highscore(self):
         highscores = self.puu.lue_highscore()
         print(highscores)
@@ -29,6 +41,13 @@ class HirsipuuSovellus:
         max_vaaria_arvauksia = 6
 
         while True:
+            self.tyhjenna_naytto()
+
+            print("---------------")
+            print("Väärin arvatut:", self.puu.vaarin_arvatut_kirjaimet)
+            print("Vääriä:", self.puu.vaaria_arvauksia, "/", max_vaaria_arvauksia)
+            print("---------------")
+            print("\n" * 5)
             self.tulosta_sana(self.puu.arvattava_sana)
             print("\n" * 2)
             print("debug_print", self.puu.debug_print())
@@ -38,37 +57,46 @@ class HirsipuuSovellus:
             kirjain = self.arvaus()
             self.puu.arvaa(kirjain)
 
-            print("---------------")
-            print("Väärin arvatut:", self.puu.vaarin_arvatut_kirjaimet)
-            print("Vääriä:", self.puu.vaaria_arvauksia, "/", max_vaaria_arvauksia)
-            # print("---------------")
-            # print("JÄLJELLÄ:", self.puu.jaljella_olevia_kirjaimia)
-            print("\n" * 5)
 
+            # TODO: voitto ja häviö erilliseen funktioon, ehkä sekuntikello joka heittää loputtua takaisin aloitusnäytölle
+            # Häviö
             if self.puu.vaaria_arvauksia >= max_vaaria_arvauksia:
-                print("Game over\n")
+                self.tyhjenna_naytto()
+                print("Game over!")
+                print("Oikea sana oli:", self.puu.arvattava_sana, "\n")
                 break
+
+            # Voitto
             if self.puu.jaljella_olevia_kirjaimia <= 0:
-                print("Voitto tuli\n")
+                self.tyhjenna_naytto()
+                print("Voitto tuli!\n")
+                self.__voittosana = self.puu.arvattava_sana
                 self.puu.kirjoita_highscore()
                 break
     
     def ohjeet(self):
-        print("+-+-+-+-+-+-+")
-        print("  HIRSIPUU")
-        print("+-+-+-+-+-+-+")
+        """ Tulostaa otsikon ja ohjeet """
+        if self.__voittosana == "":
+            self.__voittosana = "HIRSIPUU"
+
+        jakaja = f"+-+-{'+-' * (len(self.__voittosana) // 2)}+"
+
+        print(jakaja)
+        print(f"  {self.__voittosana.upper()}")
+        print(jakaja)
         print("1: Pelaa")
         print("2: Highscoret")
         print("3: Poistu")
+        print(jakaja)
 
     def suorita(self):
+        self.tyhjenna_naytto()
+
         while True:
             self.ohjeet()
-            print("+-+-+-+-+-+-+")
             komento = input("komento: ")
 
             if komento == "1":
-                print("\n" * 5) # tälle parempi ratkaisu?
                 self.pelaa()
             if komento == "2":
                 self.tulosta_highscore
